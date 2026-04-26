0a9ea93407cb9cf81b5b7cdf3c4a5e981d109e6b
ngcminh734 <20222384@eaut.edu.vn>
2026-04-26 10:11:57 +0700

    remove wrong folder

--- dat_tour_du_lich/lang/vi/passwords.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'reset' => 'Mật khẩu của bạn đã được đặt lại.',
-    'sent' => 'Chúng tôi đã gửi liên kết đặt lại mật khẩu vào email của bạn.',
-    'throttled' => 'Vui lòng chờ trước khi thử lại.',
-    'token' => 'Liên kết đặt lại mật khẩu không hợp lệ.',
-    'user' => 'Không tìm thấy tài khoản phù hợp với địa chỉ email này.',
-];
